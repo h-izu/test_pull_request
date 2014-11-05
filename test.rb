61cfa608ebf6899e45448293a7de1ea19cf27a9d
Hiroaki Izu <h-izu@nbsi.jp>
2014-11-05 14:23:42 +0900

    initial commit

---
+++ test.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+require 'net/http'
+require 'json'
+
+url = URI.parse('http://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp')
+req = Net::HTTP::Get.new("#{url.path}?#{url.query}")
+
+res = Net::HTTP.start(url.host, url.port) {|http|
+http.request(req)
+}
+
+parsed = JSON.parse(res.body)
+
+p parsed
+p parsed["weather"]
+
+p "現在の東京の天気"
+p parsed["weather"].first["main"]
